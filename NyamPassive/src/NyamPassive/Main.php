e6be15109d28fe47829fb245c373fd707c777ec3
SJNMaiden1234 <1234aiden@naver.com>
2016-08-11 09:41:55 +0900

    부활 추가 구현 완료

--- NyamPassive/src/NyamPassive/Main.php
+++ NyamPassive/src/NyamPassive/Main.php
@@ -1,6 +1,6 @@
 <?php 
 
-namespace RPGGame;
+namespace NyamPassive;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
@@ -9,21 +9,29 @@ use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
 use pocketmine\entity\Effect;
-use pocketmine\command\CommandSender;
-use pocketmine\command\Command;
-use RPGGame\task\MessageTask;
 use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\Player;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
 class Main extends PluginBase implements Listener{
 	private $passive,$psdb;
 	private $players, $playersdb;
+	private $cool1 = [], $cool2 = [];
+	function makeTimeStamp(){
+		$date = date("Y-m-d H:i:s");
+		$yy = substr($date, 0, 4);
+		$mm = substr($date, 5, 2);
+		$dd = substr($date, 8, 2);
+		$hh = substr($date, 11, 2);
+		$ii = substr($date, 14, 2);
+		$ss = substr($date, 17, 2);
+		return mktime($hh, $ii, $ss, $mm, $dd, $yy);
+	}
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->getLogger()->info("RPGGame");
 		@mkdir($this->getDataFolder());
 		$this->passive = new Config($this->getDataFolder()."passive.yml", Config::YAML);
 		$this->players = new Config($this->getDataFolder()."players.yml", Config::YAML);
-		$task = new MessageTask("이 서버는 RPGGame을 사용 중 입니다.");
-		$this->getServer()->getScheduler()->scheduleDelayedRepeatingTask($task,20*60*5);
 	}
 	public function onDisable(){
 		$this->passive->save();
@@ -35,22 +43,43 @@ class Main extends PluginBase implements Listener{
 		$player->sendMessage("부활후 3초간 재생효과(*8)가 붙습니다");
 	}
 	public function onKill(PlayerDeathEvent $e){
-		$victim = $e->getEntity();
-		$damager = $e->getEntity()->getLastDamageCause()->getDamager()->getName();
-		$passivea=$this->passive->get($damager);
-		if($passivea=1){
-			$effect1 = Effect::getEffect(1)->setDuration(100)->setAmplifier(2);
-			$damager->addEffect($effect1);
-			$effect2 = Effect::getEffect(5)->setDuration(100)->setAmplifier(3);
-			$damager->addEffect($effect2);
-			$effect3 = Effect::getEffect(8)->setDuration(100)->setAmplifier(5);
-			$damager->addEffect($effect3);
+		$killed = $e->getEntity();
+		if ($killed instanceof Player){
+			$cause = $killed->getLastDamageCause();
+			if ($cause instanceof EntityDamageByEntityEvent){
+				$damager = $cause->getDamager();
+				if ($damager instanceof Player){
+					//여기서부터 코드 작성 시작 killed는 죽은 사람을 뜻함.
+					$killed = $e->getEntity();
+					$killedn = $killed->getName();
+					$damagern = $damager->getName();
+					$passivea=$this->passive->get($damagern);
+					if ($passivea == 1){
+						$effect1 = Effect::getEffect(1)->setDuration(100)->setAmplifier(2);
+						$damager->addEffect($effect1);
+						$effect2 = Effect::getEffect(5)->setDuration(100)->setAmplifier(3);
+						$damager->addEffect($effect2);
+						$effect3 = Effect::getEffect(8)->setDuration(100)->setAmplifier(5);
+						$damager->addEffect($effect3);
+					}
+					//플레이어 처치시 신속2 힘3 점프강화5 패시브 구현
+					if ($passivea == 2){
+						$damager->setHealth($this->getHealth() + 10);
+					}
+					//부활 구현
+					if ($passivea == 3){
+						 $this->cool2[$killedn] = $this->makeTimeStamp();
+						 $respawnr = $this->cool1[$killedn] - $this->cool2[$killedn];
+						 if ($respawnr >= 300){
+						 	$x = $killed->getX();
+						 	$y = $killed->getY();
+						 	$z = $killed->getZ();
+						 	$killed->setSpawn($x, $y, $z);
+						 }
+					}
+				}
+			}
 		}
-		//플레이어 처치시 신속2 힘3 점프강화5 패시브 구현
-		if($passivea=2){
-			$damager->setHealth($this->getHealth() + 10);
-		}
-		//나름대로 구현은 했는데 이게 맞는건지 모르겠네요 수정 환영입니다 :)
 	}
 	public function onJoin(PlayerJoinEvent $e){
 		$pname = $e->getPlayer()->getName();
@@ -58,7 +87,7 @@ class Main extends PluginBase implements Listener{
 		$passivej = $this->passive->get($pname);
 		if (!$pjoint == true){
 			$this->players->set($e->getPlayer()->getName(),true);
-			$this->passive->set($e->getPlayer()->getName(), mt_rand(1,12));
+			$this->passive->set($e->getPlayer()->getName(), mt_rand(1,4));
 			$this->passive->save();
 			$this->players->save();
 			$passivej = $this->passive->get($pname);
@@ -70,45 +99,12 @@ class Main extends PluginBase implements Listener{
 			}
 			if ($passivej == 3){
 				$e->getPlayer()->sendMessage(TextFormat::RED."당신의 패시브 코드는 3번.");
-			}
-			if ($passivej == 4){
-				$e->getPlayer()->sendMessage(TextFormat::RED."당신의 패시브 코드는 4번.");
-			}
-			if ($passivej == 5){
-				$e->getPlayer()->sendMessage(TextFormat::RED."당신의 패시브 코드는 5번.");
-			}
-			if ($passivej == 6){
-				$e->getPlayer()->sendMessage(TextFormat::RED."당신의 패시브 코드는 6번.");
-			}
-			if ($passivej == 7){
-				$e->getPlayer()->sendMessage(TextFormat::RED."당신의 패시브 코드는 7번.");
-			}
-			if ($passivej == 8){
-				$e->getPlayer()->sendMessage(TextFormat::RED."당신의 패시브 코드는 8번.");
-			}
-			if ($passivej == 9){
-				$e->getPlayer ()->sendMessage (TextFormat::RED."당신의 패시브 코드는 9번.");
+			    $this->cool1[$pname] = $this->makeTimeStamp();
 			}
 		}
 		if (!$this->players->exists($e->getPlayer()->getName())){
 			$this->players->set($e->getPlayer()->getName(),false);
 			$this->players->save();
-		}
-	}
-	public function onCommand(CommandSender $sender,Command $command, $label,array $args){
-		$sname = $sender->getName();
-		if ($command == "패시브"){
-			switch ($args[0]){
-				case "설정":
-					if (!$sender->isOp()){
-						$sender->sendMessage("당신은 오피가 아닙니다.");
-					}else{
-						$this->setPassive($sender, $args[1]);
-						$sender->sendMessage("강제적으로 패시브가 변경되었습니다.");
-					}
-				case "정보":
-					$this->viewPassive($sender);
-			}
 		}
 	}
 	public function setPassive($player,$number){
